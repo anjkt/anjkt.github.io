2d03e3d494502c3a9db9b83909e4b4ef6f246524
Anjani Kumar Thakur <sunnyfixer3941@gmail.com>
2017-12-26 16:34:24 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,36 +1,168 @@
-jQuery(document).ready(function($){
-	var is_bouncy_nav_animating = false;
-	//open bouncy navigation
-	$('.cd-bouncy-nav-trigger').on('click', function(){
-		triggerBouncyNav(true);
-	});
-	//close bouncy navigation
-	$('.cd-bouncy-nav-modal .cd-close').on('click', function(){
-		triggerBouncyNav(false);
-	});
-	$('.cd-bouncy-nav-modal').on('click', function(event){
-		if($(event.target).is('.cd-bouncy-nav-modal')) {
-			triggerBouncyNav(false);
-		}
-	});
+(function($) {
+  
+  "use strict";
 
-	function triggerBouncyNav($bool) {
-		//check if no nav animation is ongoing
-		if( !is_bouncy_nav_animating) {
-			is_bouncy_nav_animating = true;
-			
-			//toggle list items animation
-			$('.cd-bouncy-nav-modal').toggleClass('fade-in', $bool).toggleClass('fade-out', !$bool).find('li:last-child').one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(){
-				$('.cd-bouncy-nav-modal').toggleClass('is-visible', $bool);
-				if(!$bool) $('.cd-bouncy-nav-modal').removeClass('fade-out');
-				is_bouncy_nav_animating = false;
-			});
-			
-			//check if CSS animations are supported... 
-			if($('.cd-bouncy-nav-trigger').parents('.no-csstransitions').length > 0 ) {
-				$('.cd-bouncy-nav-modal').toggleClass('is-visible', $bool);
-				is_bouncy_nav_animating = false;
-			}
-		}
-	}
-});
+  // Sticky Nav
+    $(window).on('scroll', function() {
+        if ($(window).scrollTop() > 200) {
+            $('.scrolling-navbar').addClass('top-nav-collapse');
+        } else {
+            $('.scrolling-navbar').removeClass('top-nav-collapse');
+        }
+    });
+
+    /* 
+   One Page Navigation & wow js
+   ========================================================================== */
+    //Initiat WOW JS
+    new WOW().init();
+
+    // one page navigation 
+    $('.main-navigation').onePageNav({
+            currentClass: 'active'
+    }); 
+
+    $(window).on('load', function() {
+       
+        $('body').scrollspy({
+            target: '.navbar-collapse',
+            offset: 195
+        });
+
+        $(window).on('scroll', function() {
+            if ($(window).scrollTop() > 200) {
+                $('.fixed-top').addClass('menu-bg');
+            } else {
+                $('.fixed-top').removeClass('menu-bg');
+            }
+        });
+
+    });
+
+    // Slick Nav 
+    $('.mobile-menu').slicknav({
+      prependTo: '.navbar-header',
+      parentTag: 'span',
+      allowParentLinks: true,
+      duplicate: false,
+      label: '',
+    });
+
+
+/* 
+   CounterUp
+   ========================================================================== */
+    $('.counter').counterUp({
+      time: 1000
+    });
+
+/* 
+   MixitUp
+   ========================================================================== */
+  $('#portfolio').mixItUp();
+
+/* 
+   Touch Owl Carousel
+   ========================================================================== */
+    var owl = $(".touch-slider");
+    owl.owlCarousel({
+      navigation: false,
+      pagination: true,
+      slideSpeed: 1000,
+      stopOnHover: true,
+      autoPlay: true,
+      items: 2,
+      itemsDesktop : [1199,2],
+      itemsDesktopSmall: [1024, 2],
+      itemsTablet: [600, 1],
+      itemsMobile: [479, 1]
+    });
+
+    $('.touch-slider').find('.owl-prev').html('<i class="fa fa-chevron-left"></i>');
+    $('.touch-slider').find('.owl-next').html('<i class="fa fa-chevron-right"></i>');
+
+/* 
+   Sticky Nav
+   ========================================================================== */
+    $(window).on('scroll', function() {
+        if ($(window).scrollTop() > 200) {
+            $('.header-top-area').addClass('menu-bg');
+        } else {
+            $('.header-top-area').removeClass('menu-bg');
+        }
+    });
+
+/* 
+   VIDEO POP-UP
+   ========================================================================== */
+    $('.video-popup').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+        fixedContentPos: false,
+    });
+
+
+  /* 
+   SMOOTH SCROLL
+   ========================================================================== */
+    var scrollAnimationTime = 1200,
+        scrollAnimation = 'easeInOutExpo';
+
+    $('a.scrollto').on('bind', 'click.smoothscroll', function (event) {
+        event.preventDefault();
+        var target = this.hash;
+        
+        $('html, body').stop().animate({
+            'scrollTop': $(target).offset().top
+        }, scrollAnimationTime, scrollAnimation, function () {
+            window.location.hash = target;
+        });
+    });
+
+/* 
+   Back Top Link
+   ========================================================================== */
+    var offset = 200;
+    var duration = 500;
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > offset) {
+        $('.back-to-top').fadeIn(400);
+      } else {
+        $('.back-to-top').fadeOut(400);
+      }
+    });
+
+    $('.back-to-top').on('click',function(event) {
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: 0
+      }, 600);
+      return false;
+    })
+
+/* Nivo Lightbox
+  ========================================================*/   
+   $('.lightbox').nivoLightbox({
+    effect: 'fadeScale',
+    keyboardNav: true,
+  });
+
+
+/* stellar js
+  ========================================================*/
+  $.stellar({
+    horizontalScrolling: true,
+    verticalOffset: 40,
+    responsive: true
+  });
+
+/* 
+   Page Loader
+   ========================================================================== */
+  $('#loader').fadeOut();
+
+}(jQuery));
+
